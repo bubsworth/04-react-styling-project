c7d1305003e7b314ac03b08beb2ec9756d394567
bubsworth <bubbard96@hotmail.com>
2023-12-08 15:58:33 +0000

    Update header css path

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,5 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
-import './components/Header.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
